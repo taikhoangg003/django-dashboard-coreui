49e4061d0173819cb251318c1de2af3555f550a0
etuannv <etuannv@gmail.com>
2020-03-23 09:47:00 +0700

    Add default value for future_data_ptc

--- app/models.py
+++ app/models.py
@@ -39,7 +39,7 @@ class PAData(models.Model):
 
     current_ptc = models.FloatField(verbose_name='Current PTC', default=0)
     future_ptc = models.FloatField(verbose_name='Future PTC', default=0)
-    future_ptc_date = models.CharField(verbose_name='Fureture PTC date', max_length=500, blank=True, null=True)
+    future_ptc_date = models.CharField(verbose_name='Fureture PTC date', max_length=500, default='', blank=True, null=True)
     
     ref_url = models.CharField(verbose_name='Refernce url', max_length=500, blank=True, null=True )
     
