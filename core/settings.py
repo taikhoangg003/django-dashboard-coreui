aedc897c6bf0d524083c742d92d572088be71559
etuannv <etuannv@gmail.com>
2020-04-08 16:57:49 +0700

    Not use timezone

--- core/settings.py
+++ core/settings.py
@@ -123,13 +123,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
