eda7e7a2fecbc1f12ae384aaf7a0e54da19f7a80
etuannv <etuannv@gmail.com>
2020-04-20 09:05:03 +0700

    add scraper

---
+++ covid-scrapy/datascraper/items.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+from w3lib.html import remove_tags
+from w3lib.html import replace_escape_chars
+import re
+from enum import Enum
+
+
+def removeMoneySymbol(value):
+    """remove money symbol
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        string -- output value
+    """
+    trim = re.compile(r'[^\d.,]+')
+    value = trim.sub('', value)
+    value = value.replace(",",".")
+    return value
+
+def getQuantity(value):
+    """Get quantity in a string
+    
+    Arguments:
+        value {string} -- input string
+    
+    Returns:
+        string -- quantity
+    """
+    if value:
+        value = str(value)
+        value = re.findall(r'(\d+)', value, re.MULTILINE)
+        if value:
+            value = value[0]
+            value = convertToInt(value)
+        else:
+            value = 0
+    
+    return value
+
+def getMoney(value):
+    """get money from a string
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        float -- money
+    """
+    if value is not None:
+        trim = re.compile(r'[^\d.,]+')
+        value = trim.sub('', value)
+        value = value.replace(",","")
+        return convertToFloat(value)
+    else:
+        return value
+
+def convertToInt(value):
+    """convert to int
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        float -- result
+    """
+    if value is None:
+        return value
+    try:
+        return int(value)
+    except ValueError:
+        return None
+        
+def convertToFloat(value):
+    """convert to float
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        float -- result
+    """
+    if value is None:
+        return value
+    try:
+        return float(value)
+    except ValueError:
+        return None
+
+def processText(value):
+    """process to get text, clean specifix character
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        string -- out put value
+    """
+    if value:
+        value = str(value)
+        value = replace_escape_chars(value)
+        value = remove_tags(value)
+        value = value.replace('#ft5_slash#', '/').replace('\\/', '/')
+        return value.strip()
+    else:
+        return ''
+
+def processHtml(value):
+    """process to get text, clean specifix character
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        string -- out put value
+    """
+    if value:
+        value = replace_escape_chars(value)
+        value = value.replace('#ft5_slash#', '/')
+        return value
+    else:
+        return ''
+
+def processFloat(value):
+    """process to get float value
+    
+    Arguments:
+        value {string} -- input string
+    
+    Returns:
+        float -- output value
+    """
+    if value:
+        value = str(value)
+        value = getMoney(value)
+        if value:
+            return value
+        else:
+            return 0
+    else:
+        return 0
+
+def processInt(value):
+    if value:
+        value = getQuantity(value)
+        return value
+    else:
+        return ''
+
+
+def processMoney(value):
+    """input_processor to extract money to float
+    
+    Arguments:
+        value {string} -- input value
+    
+    Returns:
+        float -- return money in float
+    """
+    if value:
+        value = getMoney(value)
+        return value
+    else:
+        return ''
+
+def processQuantity(value):
+    """input_processor to get quantity from a string
+    
+    Arguments:
+        value {string} -- Input value
+    
+    Returns:
+        int -- quantity
+    """
+    if value:
+        value = getQuantity(value)
+        return value
+    else:
+        return ''
+
+def processEmail(value):
+    emails = re.findall(r'\b[\w.-]+?@\w+?\.\w+?\b', value)
+    if emails:
+        for email in emails:
+            if email not in emails:
+                emails.append(email)
+    return emails
+
+def formatSex(string):
+    
+    if string.lower() == 'f' or string.lower() == 'female' or string.lower() == 'women':
+        return 'F'
+    elif string.lower() == 'm' or string.lower() == 'male' or string.lower() == 'men':
+        return 'M'
+    else:
+        return string
+
+def format_jr_u23_elite(string):
+    if string.lower() == 'under':
+        return 'u23'
+    else:
+        return string
+
+        
+
+class JoinNotEmpty(object):
+    def __init__(self, separator=u' '):
+        self.separator = separator
+
+    def __call__(self, values):
+        result = list(filter(None, values)) 
+        if len(result) > 0:
+            return self.separator.join(result)
+        else:
+            return ''
+        
+
+class CovidItem(scrapy.Item):
+    id=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    name=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    date=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+
+    group=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    
+    confirmed=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    confirmed_changed=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    
+    recovered=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    recovered_changed=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    
+    deaths=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    deaths_changed=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    tests=scrapy.Field(input_processor=MapCompose(processMoney), output_processor=TakeFirst())
+    
+    last_scraped=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    ref_url=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    created_at=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    created_by=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    modified_at=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    modified_by=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    scraped_key=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    table_name=scrapy.Field(input_processor=MapCompose(processText), output_processor=TakeFirst())
+    
---
+++ covid-scrapy/datascraper/pipelines.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+"""
+MIT License
+
+scrapy_mysql_pipeline: Asynchronous mysql Scrapy item pipeline
+
+Copyright (c) 2017 Iaroslav Russkykh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Requirements:
+- scrapy>=1.4.0
+- pymysql>=0.7.11
+
+"""
+
+import logging
+import pprint
+from scrapy.exceptions import DropItem
+
+from twisted.internet import defer
+from twisted.enterprise import adbapi
+import os
+from scrapy.exporters import CsvItemExporter
+
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+from pymysql.cursors import DictCursor
+from pymysql import OperationalError
+from pymysql.constants.CR import CR_SERVER_GONE_ERROR,  CR_SERVER_LOST, CR_CONNECTION_ERROR
+class MySQLPipeline(object):  #
+    
+    """
+    Defaults:
+    MYSQL_HOST = 'localhost'
+    MYSQL_PORT = 3306
+    MYSQL_USER = None
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = None
+    MYSQL_TABLE = None
+    MYSQL_MARK_SCRAPED_TABLE   : None
+    MYSQL_MARK_SCRAPED_KEY   : None
+    MYSQL_MARK_SCRAPED_VALUE   : None
+    MYSQL_UPSERT = False
+    MYSQL_RETRIES = 3
+    MYSQL_CLOSE_ON_ERROR = True
+    Pipeline:
+    ITEM_PIPELINES = {
+       'scrapy_mysql_pipeline.MySQLPipeline': 300,
+    }
+    """
+    stats_name = 'mysql_pipeline'
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def __init__(self, crawler):
+        self.stats = crawler.stats
+        self.stats.set_value('done', 0)
+        self.settings = crawler.settings
+        db_args = {
+            'host': self.settings.get('MYSQL_HOST', 'localhost'),
+            'port': self.settings.get('MYSQL_PORT', 3306),
+            'user': self.settings.get('MYSQL_USER', None),
+            'password': self.settings.get('MYSQL_PASSWORD', ''),
+            'db': self.settings.get('MYSQL_DB', None),
+            'charset': 'utf8',
+            'cursorclass': DictCursor,
+            'cp_reconnect': True,
+        }
+        self.retries = self.settings.get('MYSQL_RETRIES', 3)
+        self.close_on_error = self.settings.get('MYSQL_CLOSE_ON_ERROR', True)
+        self.upsert = self.settings.get('MYSQL_UPSERT', False)
+        self.table = self.settings.get('MYSQL_TABLE', None)
+        self.db = adbapi.ConnectionPool('pymysql', **db_args)
+
+    def close_spider(self, spider):
+        self.db.close()
+
+    @staticmethod
+    def preprocess_item(item):
+        """Can be useful with extremly straight-line spiders design without item loaders or items at all
+        CAVEAT: On my opinion if you want to write something here - you must read
+        http://scrapy.readthedocs.io/en/latest/topics/loaders.html before
+        """
+        return item
+
+    def postprocess_item(self, *args):
+        """Can be useful if you need to update query tables depends of mysql query result"""
+        if len(args) > 1:
+            if 'mark_parent_done_sql' not in args[0]:
+                self.stats.inc_value('done')
+
+    @defer.inlineCallbacks
+    def process_item(self, item, spider):
+        # Start process insert db
+        retries = self.retries
+        status = False
+        while retries:
+            try:
+                item = self.preprocess_item(item)
+                yield self.db.runInteraction(self._process_item, item)
+            except OperationalError as e:
+                if e.args[0] in (
+                        CR_SERVER_GONE_ERROR,
+                        CR_SERVER_LOST,
+                        CR_CONNECTION_ERROR,
+                ):
+                    retries -= 1
+                    logger.info('%s %s attempts to reconnect left', e, retries)
+                    self.stats.inc_value('{}/reconnects'.format(self.stats_name))
+                    continue
+                logger.exception('%s', pprint.pformat(item))
+                self.stats.inc_value('{}/errors'.format(self.stats_name))
+            except Exception:
+                logger.exception('%s', pprint.pformat(item))
+                self.stats.inc_value('{}/errors'.format(self.stats_name))
+            else:
+                status = True  # executed without errors
+            break
+        else:
+            if self.close_on_error:  # Close spider if connection error happened and MYSQL_CLOSE_ON_ERROR = True
+                spider.crawler.engine.close_spider(spider, '{}_fatal_error'.format(self.stats_name))
+        self.postprocess_item(item, status)
+        
+        if self.stats.get_value('done') % 10 == 0:
+            logger.info("--> Done %s/ %s", self.stats.get_value('done'), self.stats.get_value('total'))
+        
+        yield item
+
+    def _generate_sql(self, data):
+        table_name = self.table
+        if 'table_name' in data:
+            if data['table_name'] != '':
+                table_name = data['table_name']
+            
+
+        columns = lambda d: ', '.join(['`{}`'.format(k) for k in d])
+        values = lambda d: [v for v in d.values()]
+        placeholders = lambda d: ', '.join(['%s'] * len(d))
+        if self.upsert:
+            sql_template = 'INSERT INTO `{}` ( {} ) VALUES ( {} ) ON DUPLICATE KEY UPDATE {}'
+            on_duplicate_placeholders = lambda d: ', '.join(['`{}` = %s'.format(k) for k in d])
+            return (
+                sql_template.format(
+                    table_name, columns(data),
+                    placeholders(data), on_duplicate_placeholders(data)
+                ),
+                values(data) + values(data)
+            )
+        else:
+            sql_template = 'INSERT INTO `{}` ( {} ) VALUES ( {} )'
+            return (
+                sql_template.format(table_name, columns(data), placeholders(data)),
+                values(data)
+            )
+
+    def _process_item(self, tx, row):
+        if 'mark_parent_done_sql' in row:
+            try:
+                # Mark parent done
+                tx.execute(row['mark_parent_done_sql'], [])
+                return
+            except Exception:
+                logger.error("Mark done SQL: %s", row['mark_parent_done_sql'])
+                raise
+        
+        # Insert or update data in database
+        sql, data = self._generate_sql(row)
+        # logger.info(sql)
+        try:
+            tx.execute(sql, data)
+
+        except Exception:
+            logger.error("SQL: %s", sql)
+            # logger.error("SQL: %s", upsql)
+            raise
+        self.stats.inc_value('{}/saved'.format(self.stats_name))
+
+
+
+class CsvPipeline(object):  #
+    
+    stats_name = 'csvpipeline'
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def __init__(self, crawler):
+        self.stats = crawler.stats
+        self.stats.set_value('done', 0)
+        self.settings = crawler.settings
+        
+    def open_spider(self, spider):
+        if not os.path.exists(spider.scraped_key):
+            os.makedirs(spider.scraped_key)
+        
+        self.file = open(f'./{spider.scraped_key}/result.csv', 'w+b')
+        self.exporter = CsvItemExporter(self.file, include_headers_line=True, fields_to_export=self.settings.get('EXPORT_FIELDS', None))
+        
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+
+    # @defer.inlineCallbacks
+    def process_item(self, item, spider):
+        self.stats.inc_value('done')
+        self.exporter.export_item(item)
+        if self.stats.get_value('done') % 10 == 0:
+            logger.info("--> CSV pineline: Done %s/ %s", self.stats.get_value('done'), self.stats.get_value('total'))
+        
+        return item
+        
+        
