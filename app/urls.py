6128b4f7bbdd2b00f2115c63159a48cb0dce5f51
taikhoangg003 <55939498+taikhoangg003@users.noreply.github.com>
2020-10-27 15:57:28 +0700

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,29 +1,29 @@
 # -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
 
-from django.urls import path, re_path
-from app import views
+# from django.urls import path, re_path
+# from app import views
 
-urlpatterns = [
-    # Matches any html file 
-    re_path(r'^.*\.html', views.pages, name='pages'),
+# urlpatterns = [
+#     # Matches any html file 
+#     re_path(r'^.*\.html', views.pages, name='pages'),
 
-    # The home page
-    # path('', views.index, name='home'),
-    path('', views.DashboardView.as_view(), name='home'),
-    path('pa/list', views.PADataListView.as_view(), name='pa_list'),
-    path('pa/detail/<pk>', views.PADataDetailView.as_view(), name='pa_detail'),
+#     # The home page
+#     # path('', views.index, name='home'),
+#     path('', views.DashboardView.as_view(), name='home'),
+#     path('pa/list', views.PADataListView.as_view(), name='pa_list'),
+#     path('pa/detail/<pk>', views.PADataDetailView.as_view(), name='pa_detail'),
     
-    path('ohio/list', views.OhioListView.as_view(), name='ohio_list'),
-    path('ohio/detail/<pk>', views.OhioDetailView.as_view(), name='ohio_detail'),
+#     path('ohio/list', views.OhioListView.as_view(), name='ohio_list'),
+#     path('ohio/detail/<pk>', views.OhioDetailView.as_view(), name='ohio_detail'),
 
-    path('ohio-gas/list', views.OhioGasListView.as_view(), name='ohio_gas_list'),
-    path('ohio-gas/detail/<pk>', views.OhioGasDetailView.as_view(), name='ohio_gas_detail'),
+#     path('ohio-gas/list', views.OhioGasListView.as_view(), name='ohio_gas_list'),
+#     path('ohio-gas/detail/<pk>', views.OhioGasDetailView.as_view(), name='ohio_gas_detail'),
 
-    path('p2choose/list', views.P2ChooseListView.as_view(), name='p2choose_list'),
-    path('p2choose/detail/<pk>', views.P2ChooseDetailView.as_view(), name='p2choose_detail'),
+#     path('p2choose/list', views.P2ChooseListView.as_view(), name='p2choose_list'),
+#     path('p2choose/detail/<pk>', views.P2ChooseDetailView.as_view(), name='p2choose_detail'),
     
-]
+# ]
+
+def simple(request):
+    context = {}
+    return render(request, 'base.html', context)
